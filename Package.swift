c7d04769e34cb4e635f5a736a7c45c155885a1cf
Daniel Tull <dt@danieltull.co.uk>
2024-06-30 20:52:54 +0100

    Add a Detail view

--- Package.swift
+++ Package.swift
@@ -4,6 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "DesignSystem",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .macCatalyst(.v17),
+        .tvOS(.v17),
+        .visionOS(.v1),
+        .watchOS(.v10),
+    ],
     products: [
         .library(name: "DesignSystem", targets: ["DesignSystem"]),
     ],
