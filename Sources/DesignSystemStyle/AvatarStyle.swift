3bf1c86c7ffbc28e91c8d181b00307d7cdbe8804
Daniel Tull <dt@danieltull.co.uk>
2024-07-02 18:25:09 +0100

    Allow different sizes of avatar

--- Sources/DesignSystemStyle/AvatarStyle.swift
+++ Sources/DesignSystemStyle/AvatarStyle.swift
@@ -2,10 +2,20 @@ import DesignSystem
 import SwiftUI
 
 extension AvatarStyle where Self == StandardAvatarStyle {
-    public static var standard: Self { Self() }
+
+    public static var small: Self { standard(size: 25, font: 12.5, inset: 2) }
+    public static var medium: Self { standard(size: 50, font: 25, inset: 4) }
+    public static var large: Self { standard(size: 100, font: 50, inset: 8) }
+
+    fileprivate static func standard(size: CGFloat, font: CGFloat, inset: CGFloat) -> Self {
+        StandardAvatarStyle(size: size, font: font, inset: inset)
+    }
 }
 
 public struct StandardAvatarStyle: AvatarStyle {
+    let size: CGFloat
+    let font: CGFloat
+    let inset: CGFloat
 
     public func makeBody(configuration: Configuration) -> some View {
 
@@ -14,24 +24,40 @@ public struct StandardAvatarStyle: AvatarStyle {
                 .foregroundColor(.black)
 
             Circle()
-                .inset(by: 8)
+                .inset(by: inset)
                 .foregroundColor(.orange)
 
             configuration.content
                 .foregroundColor(.black)
                 .textCase(.uppercase)
-                .font(.system(size: 50))
+                .font(.system(size: font))
         }
-        .frame(width: 100, height: 100)
+        .frame(width: size, height: size)
     }
 }
 
 #Preview("AvatarStyle") {
-    HStack(spacing: 25) {
-        Avatar()
-        Avatar(initials: "DT")
-        Avatar(image: Image(systemName: "photo.circle.fill"))
+    VStack {
+        HStack(spacing: 25) {
+            Avatar()
+            Avatar(initials: "DT")
+            Avatar(image: Image(systemName: "photo.circle.fill"))
+        }
+        .avatarStyle(.small)
+
+        HStack(spacing: 25) {
+            Avatar()
+            Avatar(initials: "DT")
+            Avatar(image: Image(systemName: "photo.circle.fill"))
+        }
+        .avatarStyle(.medium)
+
+        HStack(spacing: 25) {
+            Avatar()
+            Avatar(initials: "DT")
+            Avatar(image: Image(systemName: "photo.circle.fill"))
+        }
+        .avatarStyle(.large)
     }
-    .frame(width: 400, height: 200)
-    .avatarStyle(.standard)
+    .frame(width: 400, height: 500)
 }
